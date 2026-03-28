cf378d24198eb22d785d7111925aa89577e169b9
FatihUYSAL <uysal43fatih@gmail.com>
2026-03-28 03:37:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-document.querySelectorAll('nav a').forEach(link => {
-    link.addEventListener('click', function(e) {
-        e.preventDefault();
-        const targetId = this.getAttribute('href');
-        const targetSection = document.querySelector(targetId);
-        
-        window.scrollTo({
-            top: targetSection.offsetTop - 100,
-            behavior: 'smooth'
-        });
-    });
-});
+
